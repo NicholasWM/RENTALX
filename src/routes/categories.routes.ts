1838d2bb9e7d61d94d211cb159fc94deabe6426c
NicholasWM <nicholaswesleymazzei@gmail.com>
2021-12-19 12:06:41 -0300

    add: Import file CSV Category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,15 +1,24 @@
 import { Router } from "express";
+const multer = require("multer");
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
+import { importCategorysController } from "../modules/cars/useCases/importCategory";
 
 const categoriesRoutes = Router()
+const upload = multer({
+    dest: './tmp',
 
-categoriesRoutes.get('/', (req, res)=>{
+})
+categoriesRoutes.get('/', (req, res) => {
     return listCategoriesController.handle(req, res)
 });
 
-categoriesRoutes.post('/', (req, res)=>{
+categoriesRoutes.post('/', (req, res) => {
     return createCategoryController.handle(req, res)
 });
 
-export {categoriesRoutes}
+categoriesRoutes.post('/import', upload.single('file'), (req, res) => {
+    return importCategorysController.handle(req, res)
+});
+
+export { categoriesRoutes }
